b8d480f03b47eb0c7e73e5c658d283fb60a3042f
Lucas <mac@MacOnes-MacBook-Pro.local>
2020-04-08 23:12:26 +0700

    push new

--- CV/js/script.js
+++ CV/js/script.js
@@ -1,24 +1,46 @@
 var cv = {
     init:function(){
         this.stickeyCard();
+        this.tabs();
     },
     stickeyCard:function(){
         let card = document.querySelector('.card');
         let navScroll = document.querySelector('.nav-scroll');
+        let mainContent = document.querySelector('.main-content');
         let stickey = card.offsetTop;
-        let widthOfcard = (card.offsetWidth)/10;
-        let widthOfNavScroll = (navScroll.offsetWidth)/10;
+        console.log(stickey)
         window.addEventListener('scroll',()=>{
             if(window.pageYOffset > stickey){
                 card.classList.add('stickey')
                 navScroll.classList.add('stickey')
-                card.style.width = `${widthOfcard}rem`;
-                navScroll.style.width = `${widthOfNavScroll}rem`;
+                mainContent.classList.add('stickey')
             }else{
                 card.classList.remove('stickey');
                 navScroll.classList.remove('stickey')
+                mainContent.classList.remove('stickey')
             }
         })
+    },
+
+    tabs:function(){
+        let tabLinks = document.querySelectorAll('.nav-tab__link')
+        let tabContents = document.querySelectorAll('.tab-content')
+        console.log(tabLinks)
+        tabLinks.forEach((tabLink,index)=>{      
+            tabLink.addEventListener('click',(e)=>{
+                e.preventDefault();
+                tabLinks.forEach((tab)=>{
+                    tab.classList.remove('active')
+                })
+                tabContents.forEach((content)=>{
+                    content.classList.remove('active')
+                })
+                tabLinks[index].classList.add('active')
+                tabContents[index].classList.add('active')
+            })
+           
+        })
+      
     }
 }
 cv.init();
